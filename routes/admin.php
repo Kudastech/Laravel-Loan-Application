a1c23a0b0095e618a8e6fcdfd2e1e5feffbd646f
Kudafoke Isaiah Semeton <150586151+Kudastech@users.noreply.github.com>
2024-02-07 00:56:23 +0100

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -1,1 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AdminController;
+
+Route::get('/admin', [AdminController::class, 'admindashboard']);
+
+Route::get('/show_user', [AdminController::class, 'show_user']);
+
+Route::get('/delivered/{id}', [AdminController::class, 'delivered']);
 
